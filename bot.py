a4be2f6fda7d841919ed0ced316f1dffc6c14e29
StarshinePony <128289655+StarshinePony@users.noreply.github.com>
2023-05-09 09:32:49 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,57 @@
+from discord.ext import commands
+import discord
+BOT_TOKEN = "YOUR TOKEN HERE"
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Bot is ready!")
+
+@bot.hybrid_command()
+@commands.has_role(<MODROLE>)
+async def echo(ctx, channel: discord.TextChannel, *, message):
+    await channel.send(message)
+
+@bot.hybrid_command()
+async def luna(ctx):
+    await ctx.send("HAIL LUNA")
+
+@bot.command(name='prefix')
+@commands.has_role(<MODROLE>)
+async def change_prefix(ctx, new_prefix: str):
+    bot.command_prefix = new_prefix
+    await ctx.send(f'Prefix has been changed to `{new_prefix}`')
+
+@bot.command()
+async def pick(ctx, search_term: str):
+    client_id = 'ID HERE'
+    client_secret = 'SECRET HERE'
+
+    token_url = 'https://www.deviantart.com/oauth2/token'
+    payload = {'grant_type': 'client_credentials', 'client_id': client_id, 'client_secret': client_secret}
+    response = requests.post(token_url, data=payload)
+
+    if response.status_code == 200:
+        access_token = json.loads(response.content.decode('utf-8'))['access_token']
+
+        url = f'https://www.deviantart.com/api/v1/oauth2/browse/popular?q={search_term}&limit=1'
+        headers = {'Authorization': f'Bearer {access_token}', 'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = json.loads(response.content.decode('utf-8'))
+            if data['has_more']:
+                await ctx.send(f'More than one image found for "{search_term}"')
+            elif len(data['results']) == 0:
+                await ctx.send(f'No images found for "{search_term}"')
+            else:
+                image_url = data['results'][0]['content']['src']
+                await ctx.send(image_url)
+        else:
+            await ctx.send(f'Could not search for images on DeviantArt')
+    else:
+        await ctx.send(f'Could not authenticate with DeviantArt API')
+
+
+bot.run(BOT_TOKEN)
